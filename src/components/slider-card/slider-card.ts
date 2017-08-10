96049f32f56114f5e7b8d84e5f1d3d68708d94bf
Michael Reda <michaelreda96@gmail.com>
2017-08-10 14:04:54 +0200

    albums and categories now work

--- src/components/slider-card/slider-card.ts
+++ src/components/slider-card/slider-card.ts
@@ -1,5 +1,6 @@
+import { RequestOptions, Http, Headers } from '@angular/http';
 import { PlaylistPage } from './../../pages/playlist/playlist';
-import { Slides, NavController } from 'ionic-angular';
+import { Slides, NavController, AlertController, ToastController } from 'ionic-angular';
 import { Component, ViewChild, Input } from '@angular/core';
 
 /**
@@ -18,27 +19,73 @@ export class SliderCardComponent {
   slides_start: boolean = true;
   @Input() array: any;
   @Input() title: string;
-  view_more:boolean=false;
-  constructor(private navCtrl : NavController) {
+  view_more: boolean = false;
+  constructor(private navCtrl: NavController, private http: Http, public alertCtrl: AlertController,public toastCtrl:ToastController) {
 
   }
 
-   icon_url(item) {
+  icon_url(item) {
     if (item.images || item.icons) {
-      if (item.type=="playlist"||item.type=="album") {
+      if (item.type == "playlist" || item.type == "album") {
         return 'url(' + item.images[0].url + ')';
       } else {
         return 'url(' + item.icons[0].url + ')';
       }
-    }else{
+    } else {
       return 'url(' + "http://www.freeiconspng.com/uploads/music-red-symbol-free-icon-27.png" + ')';
     }
 
   }
 
-  open_page(item){
-    if(item.type=="playlist"||"album")
-      this.navCtrl.push(PlaylistPage,item);//passing a playlist
+  open_page(item) {
+    if (item.type == "playlist" || item.type == "album") {
+      console.log("hi");
+      this.navCtrl.push(PlaylistPage, item);//passing a playlist
+    } else {
+      let headers = new Headers();
+      headers.append("Content-Type", 'application/x-www-form-urlencoded;charset=utf8');
+      headers.append("Authorization", "Bearer " + localStorage.getItem('access_token'));
+      let options = new RequestOptions({ headers: headers });
+      this.http.get(item.href + "/playlists", options).subscribe(data => {
+        var res = JSON.parse(data['_body']);
+        let playlists = res.playlists.items;
+        let inputs = [];
+        for (var playlist of playlists) {
+          inputs.push({
+            type: 'radio',
+            label: playlist.name,
+            value: playlist.id
+          })
+        }
+        this.alertCtrl.create({
+          title: 'Select a playlist',
+          inputs: inputs,
+          buttons: [
+            {
+              text: 'Cancel',
+              role: 'cancel',
+              handler: data => {
+
+              }
+            },
+            {
+              text: 'Play',
+              handler: data => {
+                console.log(data);
+                if (!data) {
+                  this.toastCtrl.create({
+                    message: 'Please select a playlist',
+                    duration: 3000,
+                  }).present();
+                }
+                this.navCtrl.push(PlaylistPage, {id:data,type:"playlist"});
+              }
+            }
+          ]
+        }).present();
+
+      });
+    }
 
   }
 }
--- src/pages/playlist/playlist.ts
+++ src/pages/playlist/playlist.ts
@@ -26,11 +26,10 @@ export class PlaylistPage {
   playlist: any;
   auto_play: string = "autoplay";
   i = 0;
-  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http, public playerP: PlayerProvider,public loader:LoaderProvider) {
-
-    loader.start('loading playlist...');
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: Http, public playerP: PlayerProvider, public loader: LoaderProvider) {
 
     this.playlist = navParams.data;
+    loader.start('loading '+this.playlist.type+'...');
     this.get_tracks();
 
 
@@ -55,19 +54,56 @@ export class PlaylistPage {
     headers.append("Authorization", "Bearer " + localStorage.getItem('access_token'));
     let options = new RequestOptions({ headers: headers });
     while (!this.playlist);
-    this.http.get("https://api.spotify.com/v1/users/spotify/playlists/" + this.playlist.id + "/tracks", options).subscribe(data => {
-      var res = JSON.parse(data['_body']);
-      this.tracks = res.items;
-      var j = 0;
-      for (j = 0; j < 10; j++) {
-        if (this.tracks[this.i + j] != undefined)
-          this.displayed_tracks.push(this.tracks[this.i + j]);
-      }
-      this.i += j;
-      console.log(this.tracks);
-      if (localStorage.getItem('auto_play') == "true")
-        this.play_tracks();
-    });
+    if (this.playlist.type == "playlist") {
+            this.http.get("https://api.spotify.com/v1/users/spotify/playlists/" + this.playlist.id + "/tracks", options).subscribe(data => {
+              var res = JSON.parse(data['_body']);
+              this.tracks = res.items;
+              var j = 0;
+              for (j = 0; j < 10; j++) {
+                if (this.tracks[this.i + j] != undefined)
+                  this.displayed_tracks.push(this.tracks[this.i + j]);
+              }
+              this.i += j;
+              console.log(this.tracks);
+              if (localStorage.getItem('auto_play') == "true")
+                this.play_tracks();
+            });
+    }else if(this.playlist.type== "album"){
+          this.http.get("https://api.spotify.com/v1/albums/"+this.playlist.id+"/tracks", options).subscribe(data => {
+              var res = JSON.parse(data['_body']);
+              this.tracks= [];
+              for(let track of res.items){
+                this.tracks.push({"track":track});
+              }
+              var j = 0;
+              for (j = 0; j < 10; j++) {
+                if (this.tracks[this.i + j] != undefined)
+                  this.displayed_tracks.push(this.tracks[this.i + j]);
+              }
+              this.i += j;
+              console.log(this.tracks);
+              if (localStorage.getItem('auto_play') == "true")
+                this.play_tracks();
+            });
+    }else{
+        this.http.get(this.playlist.href+"/playlists", options).subscribe(data => {
+              var res = JSON.parse(data['_body']);
+              console.log(res);
+              this.tracks= [];
+              for(let track of res.items){
+                this.tracks.push({"track":track});
+              }
+              var j = 0;
+              for (j = 0; j < 10; j++) {
+                if (this.tracks[this.i + j] != undefined)
+                  this.displayed_tracks.push(this.tracks[this.i + j]);
+              }
+              this.i += j;
+              console.log(this.tracks);
+              if (localStorage.getItem('auto_play') == "true")
+                this.play_tracks();
+            });
+    }
   }
 
 
@@ -98,6 +134,9 @@ export class PlaylistPage {
   }
 
   icon_url(item) {
+    if(!item){
+      return 'url(' + this.playlist.images[2].url + ')';
+    }
     if (item.images && item.images.length != 0) {
       return 'url(' + item.images[0].url + ')';
     } else {
