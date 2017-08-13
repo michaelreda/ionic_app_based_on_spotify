d17ef2d461b364837b6e66c6f4f44e6695cc8866
Michael Reda <michaelreda96@gmail.com>
2017-08-13 14:21:26 +0200

    fading in and out music in player

--- src/components/player/player.ts
+++ src/components/player/player.ts
@@ -1,6 +1,7 @@
 import { PlayerProvider } from './../../providers/player/player';
 import { Component } from '@angular/core';
 import { ToastController } from "ionic-angular";
+import { Observable } from "rxjs/Rx";
 
 /**
  * Generated class for the PlayerComponent component.
@@ -13,13 +14,13 @@ import { ToastController } from "ionic-angular";
   templateUrl: 'player.html'
 })
 export class PlayerComponent {
-  track:any;
+  track: any;
   current_track: any;
   auto_play: string = "autoplay";
-  track_details:boolean=false;
-  constructor(public playerP: PlayerProvider,private toastCtrl: ToastController) {
-    
-    
+  track_details: boolean = false;
+  constructor(public playerP: PlayerProvider, private toastCtrl: ToastController) {
+
+
     if (localStorage.getItem('auto_play') && localStorage.getItem('auto_play') == "false")
       this.auto_play = "";
 
@@ -32,23 +33,29 @@ export class PlayerComponent {
         //   duration: 3000,
         // }).present();
         this.playerP.play_next_track();
-      }else{
-        this.current_track=track.preview_url;
-        this.track=track;
+      } else {
+        this.current_track = track.preview_url;
+        this.track = track;
+        if (document.getElementById("audio")) {
+          var audio: any = document.getElementById("audio");
+          audio.volume = 0;
+          this.audio_fadein(audio);
+        }
       }
     });
   }
 
-  song_ended(){
-      this.playerP.current_track_ended.emit();
+  song_ended() {
+    this.playerP.current_track_ended.emit();
+
   }
 
-  clicked(){
+  clicked() {
     console.log(this.track);
-    this.track_details=!this.track_details;
+    this.track_details = !this.track_details;
   }
   icon_url(item) {
-    if(!item){
+    if (!item) {
       return 'url(' + this.track.images[2].url + ')';
     }
     if (item.images && item.images.length != 0) {
@@ -58,4 +65,31 @@ export class PlayerComponent {
     }
 
   }
+
+  time_updated() {
+    var audio: any = document.getElementById("audio");
+    // console.log(audio.currentTime);
+    if (audio.currentTime > 27) {
+      this.audio_fadeout(audio);
+    }
+  }
+
+  audio_fadeout(audio: any) {
+    var o = Observable.interval(375).subscribe(x => {
+      if (audio.volume - 0.125 > 0)
+        audio.volume -= 0.125;
+      else
+        o.unsubscribe();
+    });
+  }
+
+  audio_fadein(audio: any) {
+    var o = Observable.interval(375).subscribe(x => {
+      if (audio.volume + 0.1 < 1)
+        audio.volume += 0.1;
+      else
+        o.unsubscribe();
+    });
+  }
+
 }
